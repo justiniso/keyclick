f2ff953e464c9ec30fed9aca030b4183b33a6066
Justin Iso <justin.james.iso@gmail.com>
2012-09-08 00:56:30 -0400

    moved several properties and methods to a new keyclick object variable

--- keyclick.js
+++ keyclick.js
@@ -1,5 +1,5 @@
 
-var active = false;
+//var active = false;
 var presses = 0;
 var searchText = "";
 var inputElements = document.getElementsByTagName('input');
@@ -10,45 +10,116 @@ var targetElement;
 var targetLeft = 0;
 var targetTop = 0;
 var tagText = "";
-var pattern;
-var highlight = document.createElement('div');
+var activationKeycode = 192;	// tilda (192)
+
+
 var helper = document.createElement('span');
 var helperBar = document.createElement('div');
+var highlight = document.createElement('div');
 
-document.body.appendChild(highlight);
-highlight.style.width = '10px';
-highlight.style.height = '10px';
-highlight.style.position = 'absolute';
-highlight.style.backgroundColor = 'yellow';
-highlight.style.border = '3px solid orange';
-highlight.style.borderRadius = '5px';
-highlight.style.MozBorderRadius = '5px';
-highlight.style.opacity = '0.6';
-highlight.style.MozOpacity = '0.6';
-highlight.style.zIndex = '2147483647';
-highlight.style.visibility = 'hidden';
 
-document.body.appendChild(helper);
-helper.style.position = 'fixed';
-helper.style.height = '15px';
-helper.style.left = '10px';
-helper.style.top = '5px';
-helper.style.border = '1px solid #d5d5d5';
-helper.style.backgroundColor = '#ffffff';
-helper.style.fontSize = '16px';
-helper.style.zIndex = '2147483647';
+var keyclick = {
 
-document.body.appendChild(helperBar);
-helperBar.style.width = '100%';
-helperBar.style.height = '25px';
-helperBar.style.top = '0px';
-helperBar.style.position = 'fixed';
-helperBar.style.border = '1px solid #c5c5c5';
-helperBar.style.backgroundColor = 'rgb(225, 200, 135)';
-helperBar.style.opacity = '0.6';
-helperBar.style.MozOpacity = '0.6';
-helperBar.style.zIndex = '2147483646';
-helperBar.style.visibility = 'hidden';
+	active: false,
+	searchText: '',
+	matchingElements: [],
+	targetElement: {
+		node: null,
+		index: 0,		// target element's index in matchingElements array
+	},
+
+
+
+	activate: function(){
+		// Functionality
+		this.active = true;
+
+		// UI
+		this.showUI();
+	},
+
+	deactivate: function(){
+		// Functionality
+		this.active = false;
+
+		// Retain between sessions
+		this.searchText = this.searchText;
+		this.matchingElements = this.matchingElements;
+		this.targetElement = this.targetElement;
+
+		// UI
+		this.hideUI();
+	},
+
+	showUI: function(){
+		helper.style.visibility = 'visible';
+		helperBar.style.visibility = 'visible';
+		highlight.style.visibility = 'visible';
+	},
+
+	hideUI: function(){
+		helper.style.visibility = 'hidden';
+		helperBar.style.visibility = 'hidden';
+		highlight.style.visibility = 'hidden';
+	},
+
+	resetSearchText: function(){
+		this.searchText = '';
+		this.matchingElements = '';
+
+		// UI
+		helper.innerHTML = '';
+		highlight.style.visibility = 'hidden';
+	},
+
+	resetAll: function(){
+		this.resetSearchText();
+		this.deactivate();
+	},
+
+	updateAll: function(){
+
+	},
+
+	initElements: function() {
+		document.body.appendChild(highlight);
+		highlight.style.width = '10px';
+		highlight.style.height = '10px';
+		highlight.style.position = 'absolute';
+		highlight.style.backgroundColor = 'yellow';
+		highlight.style.border = '3px solid orange';
+		highlight.style.borderRadius = '5px';
+		highlight.style.MozBorderRadius = '5px';
+		highlight.style.opacity = '0.6';
+		highlight.style.MozOpacity = '0.6';
+		highlight.style.zIndex = '2147483647';
+
+		document.body.appendChild(helper);
+		helper.style.position = 'fixed';
+		helper.style.height = '15px';
+		helper.style.left = '10px';
+		helper.style.top = '5px';
+		helper.style.border = '1px solid #d5d5d5';
+		helper.style.backgroundColor = '#ffffff';
+		helper.style.fontSize = '16px';
+		helper.style.zIndex = '2147483647';
+
+		document.body.appendChild(helperBar);
+		helperBar.style.width = '100%';
+		helperBar.style.height = '25px';
+		helperBar.style.top = '0px';
+		helperBar.style.position = 'fixed';
+		helperBar.style.border = '1px solid #c5c5c5';
+		helperBar.style.backgroundColor = 'rgb(225, 200, 135)';
+		helperBar.style.opacity = '0.6';
+		helperBar.style.MozOpacity = '0.6';
+		helperBar.style.zIndex = '2147483646'; // one less than highlight and helper
+
+	}
+};
+
+keyclick.initElements();
+keyclick.resetAll();
 
 function resetAll() {
 	searchText = '';
@@ -69,19 +140,16 @@ function shiftHighlight(target) {
 
 		highlight.style.top = elTop;
 		highlight.style.left = elLeft;
-		//highlight.style.left = target.offsetLeft.toString()+'px';
-		//highlight.style.top = target.offsetTop.toString()+'px';
 		highlight.style.width = target.offsetWidth.toString()+'px';
 		highlight.style.height = target.offsetHeight.toString()+'px';
 
-		/* scroll window to that element */
-		/* NOTE: findPosition returns position as string "0px" */
+		// scroll window to that element
 		window.scrollTo(parseInt(elLeft)-100, parseInt(elTop)-100);
 	}
 	
 }
 
-/*	Loop up the document until final position in document is found	*/
+//	Loop up the document until final position in document is found	*/
 function findPosition(node){
 	var curtop = 0;
 	var curleft = 0;
@@ -95,123 +163,127 @@ function findPosition(node){
 		do {
 			curtop += node.offsetTop;
 			curleft += node.offsetLeft;
-			// curtopscroll += node.offsetParent ? node.offsetParent.scrollTop : 0;
-			// curleftscroll += node.offSetParent ? node.offsetParent.scrollLeft : 0;
 		} while (node = node.offsetParent);
 
-		pos['top'] = curtop-curtopscroll;
-		pos['left'] = curleft-curleftscroll;
+		pos.top = curtop-curtopscroll;
+		pos.left = curleft-curleftscroll;
 	}
 	return pos;
 }
 
 
 document.onkeydown= function(e) {
-	/* tilda key double pressed to activate */
-	if(e.keyCode==192){
-		if(presses!=0){
-			/*	deactivate if activated	*/
-			if(active){
-				resetAll();
-				active = false;
+	
+	// activation keycode double-tapped
+	if(e.keyCode==activationKeycode){
+		if(presses!=0){	// activation key pressed already
+			if(keyclick.active){
+				keyclick.deactivate();	
+			} else {
+				keyclick.activate();	
 			}
-			/*	activate if deactivated	*/
-			else {
-				helperBar.style.visibility = 'visible';
-				active = true;
-			}
-		}
-		else {
+		} else {
 			presses = 1;
 			presses = setTimeout('presses=0;', 300);
 		}
 		return false; /* overrides brower's default behavior for these keys */
 	}
 
-	/* escape or backspace */
-	else if((e.keyCode==27 || e.keyCode==8) && active){
-		/* only override browser default action if user entered text */
-		if(searchText.length>0){
-			resetAll();
-			return false;
-		}
-	}
-	/* arrow right */
-	else if(e.keyCode==39 && active){
-		if(targetCounter>matchingElements.length+1){
-			targetCounter=0;
-		}
-		else {
-			targetCounter++;
-		}
-		targetElement = matchingElements[targetCounter];
-		shiftHighlight(targetElement);
-		return false; /* overrides brower's default behavior for these keys */
-	}
-	/* arrow left */
-	else if(e.keyCode==37 && active){
-		if(targetCounter>1){
-			targetCounter--;
-		}
-		else {
-			targetCounter = matchingElements.length-1;
-		}
-		targetElement = matchingElements[targetCounter];
-		shiftHighlight(targetElement);
-		return false; /* overrides brower's default behavior for these keys */
+	// escape or backspace
+	else if((e.keyCode==27 || e.keyCode==8) && keyclick.active){
+		keyclick.resetSearchText();
+		return false;
 	}
 
-	try {
-		targetElement = matchingElements[targetCounter];
-		shiftHighlight(targetElement);
+	// arrow right
+	else if(e.keyCode==39 && keyclick.active){
+		var i = keyclick.targetElement.index;
+
+		if(i > keyclick.matchingElements.length+1){
+			i = 0;
+		} else {
+			i++;
+		}
+
+		keyclick.targetElement.index = i;
+
+		return false;
 	}
-	catch(err){
+
+	// arrow left
+	else if(e.keyCode==37 && keyclick.active){
+		var i = keyclick.matchingElements.index;
+
+		if(i > 1){
+			i--;
+		} else {
+			i = matchingElements.length-1;
+		}
+
+		keyclick.targetElement.index = i;
+
+		return false;
+	}
+
+	// shift to the target element
+	try {
+		keyclick.targetElement.node = keyclick.matchingElements[ keyclick.targetElement.index ];
+		shiftHighlight(keyclick.targetElement.node);
+	} catch(err){
 		alert(err.message);
 	}
 }
 
 document.onkeypress= function(e) {
-	/* Enter is pressed */
-	if(e.charCode==13 && active){
 
-		if(targetElement){
-			targetElement.click();
-		}
-		else {
+	/* Enter is pressed */
+	if(e.charCode==13 && keyclick.active){
+		var target = keyclick.targetElement.node;
+
+		if(target){
+			target.click();
+		} else {
 			alert('no links found');
 		}
 
-		resetAll();
+		keyclick.resetAll();
 	}
 
-	/* Non-tilda character pressed */
-	else if(e.charCode!=96 && active) {		/* NOTE: tilda registers charCode 96, keyCode 192	*/
-		matchingElements = [];
-		searchText = searchText+String.fromCharCode(e.charCode);
-		pattern = new RegExp(searchText, 'i');
+	// Non-activation character pressed; match element to search text
+	else if(e.charCode!=activationKeycode && keyclick.active) {	
+		var pattern;
 
-		helper.innerHTML = searchText;
-		highlight.style.visibility = 'visible';
+		keyclick.matchingElements = [];	// reset matching elements
+		keyclick.searchText += String.fromCharCode(e.charCode);
 
-		/* Add all elements with matching text to matchingText array */
+		pattern = new RegExp(keyclick.searchText, 'i');	// case insensitive search pattern
+
+
+		// Add all elements with matching text to matching elements array
 		for(el in linkElements){
-			tagText = linkElements[el].innerHTML ? linkElements[el].innerHTML.toLowerCase() : '';
+			var testElement = linkElements[el];
+			var text = testElement.innerHTML ? testElement.innerHTML.toLowerCase() : '';
 
-			/* Push the element to matching array if it matches */
-			if(pattern.test(tagText)){
-				matchingElements.push(linkElements[el]);
+			// Push the element to matching array if it matches
+			if(pattern.test(text)){
+				keyclick.matchingElements.push(testElement);
 			}
 		}
 
-		/* Set target element to first matching element, if any */
-		if(matchingElements.length>0) { /* at least one element matches */
-			targetElement = matchingElements[targetCounter]; /* TODO: add in check for this*/
-			shiftHighlight(targetElement);
-		}
-		else {
-			targetElement = null;
+		// Set target element to first matching element, if any
+		if(keyclick.matchingElements.length > 0) {
+			var i = keyclick.targetElement.index;
+			var firstMatchingElement = keyclick.matchingElements[i];
+
+			keyclick.targetElement.node = firstMatchingElement; 
+			shiftHighlight(keyclick.targetElement.node);
+		} else {
+			keyclick.targetElement.node = null;
 			highlight.style.visibility = 'hidden';
 		}
+
+		helper.innerHTML = keyclick.searchText;
+		highlight.style.visibility = 'visible';
 
 		return false; /* overrides brower's default behavior for these keys */
 
